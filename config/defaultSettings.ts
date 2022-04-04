f376dbdf011d242bf2f293eb73777606d3bcb466
DESKTOP-9KA858K\17180 <1718054401@qq.com>
2022-04-04 13:42:41 +0800

    first-commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,20 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'top',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  colorWeak: false,
+  title: 'Berry Oncology',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+
+export default Settings;
